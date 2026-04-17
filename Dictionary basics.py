c6b70995136bfa6d6e48640babaa9c49792d27b4
nitishthakuur <nitishthakuur@gmail.com>
2026-04-17 11:16:55 +0530

    Add files via upload
    I moved to the dictionaries and start doing some basics

---
+++ Dictionary basics.py
@@ -0,0 +1,30 @@
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "BCA"
+}
+for key in student:
+	print(key, student[key])
+	
+	
+# Q 1
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "BCA"
+}
+print(student["name"])
+print(student["age"])
+
+# Q 2
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "BCA"
+}
+student["marks"] = 85
+print(student["marks"])
+
+student["age"] = 21
+print(student["age"])
+
